c422e94d28cd66b44e733ab58cddae79dd60e768
likesea <kukuyanbin@163.com>
2015-09-07 21:57:54 +0800

    重新添加文件

---
+++ TicketClient/TicketClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TicketClient.cs.MvcApplication" Language="C#" %>
---
+++ TicketSystem.ServiceContract/TicketSystem.ServiceContract/TicketSystemClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack;
+
+namespace TicketSystem.ServiceContract
+{
+    public class TicketSystemClient : JsonServiceClient
+    {
+        public TicketSystemClient(string baseUrl) : base(baseUrl)
+        {
+            
+        }
+
+        private static string baseUrl = System.Configuration.ConfigurationManager.AppSettings["soaUrl"];
+        public TicketSystemClient()
+            : base(baseUrl + "ServiceStackServer")
+        {
+            
+        }
+    }
+}
